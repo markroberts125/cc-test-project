d92a47c45cd94f7dccb06320353e05a7b17698a1
Mark Roberts <mark.roberts125@gmail.com>
2021-06-20 00:35:19 +0100

    Rate API tests and step defs

--- src/test/java/stepdefinitions/StepDefs.java
+++ src/test/java/stepdefinitions/StepDefs.java
@@ -5,15 +5,15 @@ import internal.RequestHandler;
 
 import javax.ws.rs.core.Response.Status;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static internal.RequestHandler.AUTH_TOKEN;
+import static internal.RequestHandler.*;
 import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class StepDefs extends CucumberConfig {
 
@@ -22,12 +22,25 @@ public class StepDefs extends CucumberConfig {
 
     @Given("a request to authenticate with the API")
     public void aRequestToAuthenticateWithTheAPI() {
-        requestHandler.buildRequest("auth");
+        requestHandler.buildRequest(AUTH_FILENAME);
+    }
+
+    @Given("a request to get a quote from the API")
+    public void aRequestToGetAQuoteFromTheAPI() {
+        requestHandler.buildRequest("quote");
     }
 
     @Given("a request to close the session with the API")
     public void aRequestToCloseTheSessionWithTheAPI() {
-        requestHandler.buildRequest("closeSession");
+        requestHandler.buildRequest(CLOSE_SESSION_FILENAME);
+    }
+
+    @Given("I want to sell {string} {string} and buy {string} using the {string} side")
+    public void iWantToSellAndBuyUsingTheSide(String amount, String sellCurrency, String buyCurrency, String side) {
+        requestHandler.getRequest().setQueryParameter("amount", amount);
+        requestHandler.getRequest().setQueryParameter("buy_currency", sellCurrency);
+        requestHandler.getRequest().setQueryParameter("sell_currency", buyCurrency);
+        requestHandler.getRequest().setQueryParameter("fixed_side", side);
     }
 
     @When("the request is sent")
@@ -49,5 +62,18 @@ public class StepDefs extends CucumberConfig {
     public void theKeyIHaveIsNoLongerValid() {
         requestHandler.sendUnauthorisedRequest();
         assertEquals(UNAUTHORIZED, requestHandler.getResponse().getStatus());
+    }
+
+    @Then("the error codes received contains {string}")
+    public void theErrorReceivedIsError(String error) {
+        assertTrue(requestHandler.getResponse().getBodyValue("$..code").contains(error));
+    }
+
+    @Then("the buy amount matches the provided rate")
+    public void theBuyAmountMatchesTheProvidedRate() {
+        float buyAmount = Integer.getInteger(requestHandler.getResponse().getBodyValue("client_buy_amount"));
+        float sellAmount = Integer.getInteger(requestHandler.getResponse().getBodyValue("client_sell_amount"));
+        float rate = Integer.getInteger(requestHandler.getResponse().getBodyValue("core_rate"));
+        assertEquals(buyAmount, (sellAmount / rate), 1);
     }
 }
--- src/test/resources/features/scenarios.feature
+++ src/test/resources/features/scenarios.feature
@@ -6,6 +6,26 @@ Feature: Using the Payments API
     Then I receive an OK response
     And I receive a key for future authentication
 
+  Scenario: Get a quote from the API
+    Given a request to get a quote from the API
+    And I want to sell "10000" "GBP" and buy "USD" using the "sell" side
+    When the request is sent
+    Then I receive an OK response
+    And the buy amount matches the provided rate
+
+  Scenario Outline: Sending invalid requests to the API
+    Given a request to get a quote from the API
+    And I want to sell "<amount>" "<sell>" and buy "<buy>" using the "<side>" side
+    When the request is sent
+    Then I receive a Bad Request response
+    And the error codes received contains "<error>"
+
+    Examples:
+      | amount | sell | buy | side | error                   |
+      | 10000  | GBP  | GBP | sell | unique_value_parameters |
+      | -10000 | GBP  | USD | sell | amount_type_is_wrong    |
+      | -10000 | GBP  | USD | sell | amount_is_too_small     |
+
   Scenario: Closing the session with the API
     Given a request to close the session with the API
     When the request is sent
